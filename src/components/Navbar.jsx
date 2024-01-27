c471f32b43e08ee0f18fe466bf5a46c4336a65e9
Artemiy <92556471+LuckyRUS38@users.noreply.github.com>
2024-01-27 20:09:10 +0800

    Based concept my portfolio

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+/////////////
+// IMPORTS //
+/////////////
+import "./navbar.scss";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      {/* Sidebar */}
+      <div className="wrapper">
+        <span>Lucky Dev</span>
+        <div className="social">
+          <a><img src="" alt=""/></a>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar;
